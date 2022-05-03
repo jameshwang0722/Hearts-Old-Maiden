0cb26d582c344dd97bb60dbc7ffe1b9709df4cf2
James Hwang <>
2022-05-03 15:31:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from Hearts import Hearts
+from OldMaid import oldmaid
+from UsernamesandPlayers import players
+
+# #Playing OldMaid
+# game = oldmaid()
+# Users = players()
+# game.setup(Users)
+# game.play_game()
+
+
+#Playing Hearts
+Users = players()
+game = Hearts(Users)
+game.setup_hearts()
+game.play_hearts()
